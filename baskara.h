e4f91782f13912f536165e3704e89f54dcc5f7e0
Carlos Ramon <ramon.ethan@gmail.com>
2017-11-29 18:41:37 -0300

    criando bibliotecas

---
+++ baskara.h
@@ -0,0 +1,3 @@
+#define delta(a,b,c) ((b)*(b)-4*(a)*(c))
+#define raiz1(a,b,nDelta) (-(b)+(nDelta)/2*(a))
+#define raiz2(a,b,nDelta) (-(b)-(nDelta)/2*(a))
---
+++ macro.h
@@ -0,0 +1,3 @@
+#define quad(n) ((n)*(n))
+#define abs(n) ((n)<0?-(n):(n))
+#define max(x,y) ((x)>(y)?(x):(y))
