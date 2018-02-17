a31e9151d7959725e305086321d918180c62c1e3
CarlosRamon <ramon.ethan@gmail.com>
2018-02-17 17:27:10 -0300

    015 finalizado

--- 015.c
+++ 015.c
@@ -7,40 +7,73 @@
 // tipo PROD e outra capaz de imprimir a estrutura.
 
 #include <stdio.h>
+#include <string.h>
+
+#define MAX 5
 
 typedef struct TProd {
-    char titulo[15];
+    char titulo[25];
     float pVenda;
 } TProd;
 
-void attPreco();
-void ordProd();
-void printList(TProd prod);
+void attPreco(TProd *aux);
+void ordProd(TProd *ord);
+void printList(TProd *prod);
 
 int main(){
-    char *pAux;
-    TProd produto[2];
+    int j=0;
+    TProd produto[MAX];
     printf("Cadastro de Produtos:\n");
-    for (int i = 0; i < 2; i++) {
-        printf("Entre com o nome do produto [%d]: ",i+1);
+    for (int i = 0; i < MAX; i++) {
+        printf("Entre com o nome do produto [%d]: ",i);
         scanf(" %[^\n]s",produto[i].titulo);
         printf("Entre com o valor para %s:",produto[i].titulo);
         scanf("%f",&produto[i].pVenda);        
     }
-    ordProd();
-    for (int i = 0; i < 2; i++) {
-        printList(produto[i]);        
+    attPreco(produto);
+    ordProd(produto);
+    printList(produto);        
+}
+
+void attPreco(TProd *aux){
+    for (int i = 0; i < MAX; i++) {
+        if (aux[i].pVenda <= 100) {
+            aux[i].pVenda += (aux[i].pVenda * 0.05);
+        }
     }
 }
 
-void attPreco(){
+void ordProd(TProd *ord){
+    int j = 0,aux2;
+    char aux[15];
+    for (int i = 0; i < MAX; i++) {
+        while(ord[i].titulo[j]!='\0'){
+            j++;
+        }
+        for (int k = 0; k < j; k++) {
+            if (ord[i].titulo[k]>=97) {
+                ord[i].titulo[k]-=32;
+            }
+        }
+    }
+    for (int j = 0; j < MAX; j++) {
+        for (int i = 0; i < (MAX-1); i++) {
+            if (strcmp(ord[i].titulo,ord[i+1].titulo)>0) {
 
+                strcpy(aux,ord[i].titulo);
+                strcpy(ord[i].titulo,ord[i+1].titulo);
+                strcpy(ord[i+1].titulo,aux);
+
+                aux2 = ord[i].pVenda;
+                ord[i].pVenda = ord[i+1].pVenda;
+                ord[i+1].pVenda = aux2;
+            }
+        }
+    }
 }
 
-void ordProd(){
-
-}
-
-void printList(TProd prod){
-        printf("[%d] - %s\t:\t%.2f.\n",i+1,prod.titulo,prod.pVenda);
+void printList(TProd *prod){
+    for (int i = 0; i < MAX; i++) {
+        printf("[%d] - %s\t\t:\t%.2f.\n",i,prod[i].titulo,prod[i].pVenda);
+    }
 }
